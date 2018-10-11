c5dfa91804cca7782b6fd96bfd17fd984e726a1a
Joel Otero <supwer00@gmail.com>
2018-10-11 12:42:53 +0200

    Add files via upload

--- P3-Exercici13/Exercici13.java
+++ P3-Exercici13/Exercici13.java
@@ -1,1 +1,8 @@
+public class Exercici13 {
 
+    public static void main(String[] args) {
+        Alumne alumne1 = new Alumne("Joel", 2.83, 4.84, 7.69, 0);
+        System.out.println(alumne1.calcularNotaFinal());
+        System.out.println(alumne1.veureNotaFinalFormatText());
+    }
+}
