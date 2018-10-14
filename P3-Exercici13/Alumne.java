74e0f286b2189d8599e7fa2f9d29d6f63fb75e29
Joel Otero <supwer00@gmail.com>
2018-10-15 01:00:23 +0200

    Update Alumne.java
    Casi terminao

--- P3-Exercici13/Alumne.java
+++ P3-Exercici13/Alumne.java
@@ -1,20 +1,20 @@
-public class Alumne {
+class Alumne {
 
-    //Inicialitzem les vaiables necesaries pel funcionament mínim de la classe Alumne
-    static double  maxPuntsExtra = 1.5;
-    String nom;
-    double notaExamen1;
-    double notaExamen2;
-    double notaExamen3;
-    double puntsExtra;
-    double notaFinal;
+    // Inicialitzem les vaiables necesaries pel funcionament mínim de la classe Alumne
+    private static double maxPuntsExtra = 1.5;
+    private String nom;
+    private double notaExamen1;
+    private double notaExamen2;
+    private double notaExamen3;
+    private double puntsExtra;
+    private double notaFinal;
 
-    //Constructor d'Alumne per instanciar un nou objecte Alumne amb un nom i les demes variables a 0.0
+    // Constructor d'Alumne per instanciar un nou objecte Alumne amb un nom i les demes variables a 0.0
     Alumne(String nom) {
         this.nom = nom;
     }
 
-    //Constructor d'Alumne per instanciar un nou objecte Alumne amb totes les variables
+    // Constructor d'Alumne per instanciar un nou objecte Alumne amb totes les variables
     Alumne(String nom, double notaExamen1, double notaExamen2, double notaExamen3, double puntsExtra) {
         this.nom = nom;
         this.notaExamen1 = notaExamen1;
@@ -23,15 +23,25 @@ public class Alumne {
         this.puntsExtra = puntsExtra;
     }
 
-    //Funció per calcular la nota final
+    // Funció per modificar la quantitat màxima de punts extra.
+    static void modificarMaxPuntsExtra(double max) {
+        maxPuntsExtra = max;
+    }
+
+    // Funció per calcular la nota final
     double calcularNotaFinal() {
 
-        notaFinal = ((notaExamen1 + notaExamen2 + notaExamen3)/3) + puntsExtra;
+        if (puntsExtra > maxPuntsExtra)
+            puntsExtra = maxPuntsExtra;
+
+        notaFinal = ((notaExamen1 + notaExamen2 + notaExamen3) / 3) + puntsExtra;
 
         return notaFinal;
     }
 
-    //Funció per veure la nota final en format text
+    // TODO Añadir la tabla de testeo de calcularNotaFinal()
+
+    // Funció per veure la nota final en format text
     String veureNotaFinalFormatText() {
         notaFinal = ((notaExamen1 + notaExamen2 + notaExamen3) / 3) + puntsExtra;
         String notaText = null;
@@ -48,9 +58,14 @@ public class Alumne {
 
         if (notaFinal < 5) {
             notaText = "Insuficient";
-            return "L'alumne " + nom + " ha tret un " + notaText +  " amb una qualificació de: " + notaFinal;
+            return "L'alumne " + nom + " ha tret un " + notaText + " amb una qualificació de: " + notaFinal;
         } else {
             return "L'alumne " + nom + " ha tret un " + notaText;
         }
     }
+
+    // TODO Añadir la tabla de testeo de veureNotaFinalFormatText()
+
+    // TODO Pregunta 13.e i 13.f
+
 }
